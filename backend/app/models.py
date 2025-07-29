4ca963b3d19d2f680235273e7e067a3ac5f1ae59
wechiwin <halocooper@outlook.com>
2025-07-29 10:27:07 +0800

    所有的数据库表增加创建时间和更新时间

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,16 +1,29 @@
 from app.database import db
+from datetime import datetime
 
 
-class Holding(db.Model):
+class TimestampMixin:
+    """
+    混入类：为继承它的所有模型统一增加
+    created_at 和 updated_at 两个时间戳字段
+    """
+    created_at = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
+    updated_at = db.Column(db.DateTime,
+                           default=datetime.utcnow,
+                           onupdate=datetime.utcnow,
+                           nullable=False)
+
+
+class Holding(TimestampMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     fund_name = db.Column(db.String(100))
     fund_code = db.Column(db.String(50), unique=True)
     fund_type = db.Column(db.String(50))
 
 
-class Transaction(db.Model):
+class Transaction(TimestampMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    fund_code = db.Column(db.Integer, db.ForeignKey('holding.fund_code'))
+    fund_code = db.Column(db.String(50), db.ForeignKey('holding.fund_code'))
     transaction_type = db.Column(db.String(10))
     transaction_date = db.Column(db.String(20))
     transaction_net_value = db.Column(db.Float)
@@ -20,7 +33,7 @@ class Transaction(db.Model):
 
 class NetValue(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    fund_code = db.Column(db.Integer, db.ForeignKey('holding.fund_code'))
+    fund_code = db.Column(db.String(50), db.ForeignKey('holding.fund_code'))
     date = db.Column(db.String(20))
     unit_net_value = db.Column(db.Float)
     accumulated_net_value = db.Column(db.Float)
